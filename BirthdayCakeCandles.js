734b044d844f82bd0e1f9aa875af9108a2f2791e
Tobi <toubieogunleye@live.com>
2019-02-21 22:44:12 +0000

    optimize algorithm

--- BirthdayCakeCandles.js
+++ BirthdayCakeCandles.js
@@ -1,19 +1,20 @@
+
+// ref: https://www.hackerrank.com/challenges/birthday-cake-candles/leaderboard
 // Complete the birthdayCakeCandles function below.
 function birthdayCakeCandles(ar) {
 
     let dict = {};
+    let max = 0
+
     for (let index = 0; index < ar.length; index++) {
         let candle = ar[index];
         if(dict[candle] == undefined){
             dict[candle] = 0;
         }
         dict[candle] += 1;
-    }
-    let max = 0
-    for (let index = 0; index < ar.length; index++) {
-        let candle = ar[index];
         max = dict[candle] > max ? dict[candle] : max;
-    }
+
+    } 
     return (max);
 }
 
